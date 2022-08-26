1442948302edd9db43e6d46455a23566e84d0e4a
zepcast <hi@zepcast.uk>
2022-08-26 15:00:21 +0100

    setup: merge nuxt 3 starter and base signature work

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,48 +3,48 @@
  * This file isn't processed by webpack. Using `~/` results in some odd runtime problems, without any obvious errors.
  */
 
- import type { Config } from 'tailwindcss'
- // @ts-expect-error
- import { pxToRem } from '@captaincss/captaincss/helpers'
- import { fontFamily, fontSize, fontWeight, lineHeight } from './config/tailwind/typography'
- import { screens } from './config/tailwind/screens'
- import { spacing } from './config/tailwind/spacing'
- import { safelist } from './config/tailwind/safelist'
- import { colors } from './config/tailwind/colors'
+import type { Config } from 'tailwindcss'
+// @ts-expect-error
+import { pxToRem } from '@captaincss/captaincss/helpers'
+import { fontFamily, fontSize, fontWeight, lineHeight } from './config/tailwind/typography'
+import { screens } from './config/tailwind/screens'
+import { spacing } from './config/tailwind/spacing'
+import { safelist } from './config/tailwind/safelist'
+import { colors } from './config/tailwind/colors'
 
- export default <Config>{
-   safelist,
-   plugins: [require('@captaincss/captaincss')],
-   prefix: 'u-',
-   theme: {
-     fontSize,
-     fontFamily,
-     fontWeight,
-     lineHeight,
-     screens,
-     spacing,
-     colors,
-   },
-   content: [
-     'app-modules/**/*.{vue,js}',
-     'layouts/**/*.vue',
-     'pages/**/*.vue',
-     'composables/**/*.{js,ts}',
-     'plugins/**/*.{js,ts}',
-     'App.{js,ts,vue}',
-     'app.{js,ts,vue}',
-   ],
-   captain: {
-     prefix: {
-       objects: 'o-',
-       components: 'c-',
-     },
-     separator: {
-       elements: '__',
-       modifiers: '--',
-     },
-     support: {
-       flexGap: true, // https://caniuse.com/flexbox-gap
-     },
-   },
- }
+export default <Config>{
+  safelist,
+  plugins: [require('@captaincss/captaincss')],
+  prefix: 'u-',
+  theme: {
+    fontSize,
+    fontFamily,
+    fontWeight,
+    lineHeight,
+    screens,
+    spacing,
+    colors,
+  },
+  content: [
+    'app-modules/**/*.{vue,js}',
+    'layouts/**/*.vue',
+    'pages/**/*.vue',
+    'composables/**/*.{js,ts}',
+    'plugins/**/*.{js,ts}',
+    'App.{js,ts,vue}',
+    'app.{js,ts,vue}',
+  ],
+  captain: {
+    prefix: {
+      objects: 'o-',
+      components: 'c-',
+    },
+    separator: {
+      elements: '__',
+      modifiers: '--',
+    },
+    support: {
+      flexGap: true, // https://caniuse.com/flexbox-gap
+    },
+  },
+}
